6ef2111516f6c7b8ea0054412a97bf46b5ea29d7
EC2 Default User <ec2-user@ip-172-31-44-209.ap-northeast-1.compute.internal>
2020-06-04 06:20:28 +0000

    #7-3 マイグレーションの修正

--- db/migrate/20200218145502_change_column_fest.rb
+++ db/migrate/20200218145502_change_column_fest.rb
@@ -1,5 +1,6 @@
 class ChangeColumnFest < ActiveRecord::Migration[5.2]
   def up
+    change_column :fests, :fest_status, :string, default: nil
     change_column :fests, :fest_status, 'integer USING CAST(fest_status AS integer)', null: false, default: 0
   end
   
